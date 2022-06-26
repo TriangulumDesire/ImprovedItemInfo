8ada000ab91e6ec02889535d7e9bc7aee8ef0985
Matthew Schafer <lucidsigma17@gmail.com>
2022-06-26 18:07:52 +1000

    Add global item to improve speed stats

---
+++ Items/Globals/GlobalItemImprovedSpeedTooltip.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ImprovedItemInfo.Items.Globals
+{
+    public class GlobalItemImprovedSpeedTooltip
+        : GlobalItem
+    {
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            if (item.damage <= 0 || Main.netMode == NetmodeID.Server)
+            {
+                return;
+            }
+
+            Player player = Main.player[item.playerIndexTheItemIsReservedFor];
+
+            foreach (TooltipLine tooltip in tooltips)
+            {
+                if (!tooltip.Name.Equals("Speed"))
+                {
+                    continue;
+                }
+
+                try
+                {
+                    string[] tooltipData = tooltip.Text.Split(' ');
+
+                    if (!tooltipData[^1].Equals("speed"))
+                    {
+                        return;
+                    }
+
+                    Item unmodifiedItem = new();
+                    unmodifiedItem.CloneDefaults(item.type);
+                    bool isCalamityModItem = item.ModItem?.Mod?.Name == "CalamityMod";
+
+                    int totalSpeed = item.useAnimation;
+                    int baseSpeed = unmodifiedItem.useAnimation;
+
+                    if (isCalamityModItem)
+                    {
+                        --totalSpeed;
+                        --baseSpeed;
+                    }
+
+                    if (item.DamageType == DamageClass.Melee || item.DamageType == DamageClass.SummonMeleeSpeed)
+                    {
+                        if (!isCalamityModItem)
+                        {
+                            totalSpeed = item.useTime;
+                            baseSpeed = unmodifiedItem.useTime;
+                        }
+
+                        float meleeModifier = 1.0f / player.GetTotalAttackSpeed(DamageClass.Melee);
+
+                        totalSpeed = (int)(Math.Round(totalSpeed * meleeModifier));
+                    }
+
+                    int speedDelta = totalSpeed - baseSpeed;
+
+                    tooltip.Text = $"{totalSpeed}";
+
+                    if (speedDelta != 0)
+                    {
+                        tooltip.Text += $" ({(speedDelta > 0 ? "+" : "-")}{Math.Abs(speedDelta)})";
+                    }
+                    
+                    for (int i = 0; i < tooltipData.Length; ++i)
+                    {
+                        tooltip.Text += ((i == 0) ? " (" : " ") + tooltipData[i] + ((i == tooltipData.Length - 2) ? ")" : "");
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+    }
+}
